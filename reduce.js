963c041963b6091dd3e2b5da3074a677ad8bcd8d
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-28 21:46:48 +0600

    reduce

--- reduce.js
+++ reduce.js
@@ -1,6 +1,7 @@
-const nums =[12, 25, 37, 29];
+const nums =[12, 25, 37, 29,20,40];
 nums.map(num => num*2);
 nums.filter(num => num> 20);
 nums.find(num => num > 23);
 nums.reduce((sum, num) => sum + num ,0)
 nums.map(num => num*3);
+nums.map(num => num/2);
